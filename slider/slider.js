8f5ce3c57abd5842335548171b05907fad8a6297
Chris LeBaron <chrislebaron@gmail.com>
2015-02-23 10:16:44 -0700

    initial commit

---
+++ slider/slider.js
@@ -0,0 +1,11 @@
+	
+	for (var cat_index in piclist) {
+		var category = piclist[cat_index];
+		for (var image_index in category) {
+			var imagepair = category[image_index];
+			var fullsize = baseurl + imagepair[0];
+			var thumbnail = baseurl + imagepair[1];
+			var mainDiv = document.getElementById('mainDiv');
+			
+		};
+	};

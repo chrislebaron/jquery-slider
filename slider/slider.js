be7ec92ff2dffa2b6d3e469e413d0b026c9c3adf
Christopher Lebaron <clebaro3@dmail.dixie.edu>
2015-02-28 11:06:51 -0700

    thumbnails on page

--- slider/slider.js
+++ slider/slider.js
@@ -1,11 +1,15 @@
-	
+$( document ).ready(function() {
+	var picHTML = '';
+	var mainDiv = document.getElementById('mainDiv');
 	for (var cat_index in piclist) {
 		var category = piclist[cat_index];
 		for (var image_index in category) {
 			var imagepair = category[image_index];
 			var fullsize = baseurl + imagepair[0];
 			var thumbnail = baseurl + imagepair[1];
-			var mainDiv = document.getElementById('mainDiv');
+			picHTML += '<img src="' + thumbnail + '" >';
 			
 		};
 	};
+	mainDiv.innerHTML = picHTML;
+});
